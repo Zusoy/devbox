14cd9d420b37097298fde2f27978dd9c44241e16
Zusoy <gregoire.drapeau79@gmail.com>
2023-10-27 22:50:51 +0200

    Clean app code

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -42,7 +42,7 @@ const indexHtml = join(process.env.DIST, 'index.html')
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Main window',
+    title: 'Devbox',
     icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
     webPreferences: {
       preload,
